cf72d820072dc9156df146b99e1e3b1d9c5f9606
Mehedi Hasan <hellomehediwd@gmail.com>
2023-08-11 10:39:53 +0600

    Write a converter function to make tags from string

--- js/script.js
+++ js/script.js
@@ -13,3 +13,9 @@ const numbers = [1, 2, 3, 4, 5, 6];
 const sum = numbers.reduce((acc, cur) => acc + cur, 0);
 
 console.log(sum);
+
+const tagsInString =
+  "shirt shirts pant pants watch watches tshirt tshirts t-shirt t-shirts";
+
+const tagsArray = tagsInString.split(" ");
+console.log(tagsArray);
